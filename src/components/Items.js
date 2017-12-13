69e484f32044b31fdcd3a0fe9dcdbd8c39ddede7
amite <amit.erandole@gmail.com>
2017-12-13 20:46:21 +0530

    added callbacks to update state

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
-import Item from './Item';
-import Filter from './Filter';
+import React, { Component } from 'react'
+import Item from './Item'
+import Filter from './Filter'
 
 class Items extends Component {
   state = {
-    // What state does this component have?
-  };
+    searchTerm: ''
+  }
 
-  updateSearchTerm = searchTerm => {};
+  updateSearchTerm = searchTerm => {
+    this.setState({ searchTerm })
+  }
 
   render() {
-    const { title, items } = this.props;
+    const { title, items, onToggle, onRemove } = this.props
     return (
       <section className="Items">
         <h2>
           {title} ({items.length})
         </h2>
-        <Filter searchTerm={''} onChange={this.updateSearchTerm} />
+        <Filter
+          searchTerm={this.state.searchTerm}
+          onChange={this.updateSearchTerm}
+        />
         {items
           .filter(item =>
-            // Hmm… this needs some work.
-            item.value.toLowerCase().includes(''.toLowerCase()),
+            item.value
+              .toLowerCase()
+              .includes(this.state.searchTerm.toLowerCase())
           )
           .map(item => (
             <Item
               key={item.id}
+              onToggle={onToggle}
               onCheckOff={() => {}}
-              onRemove={() => {}}
+              onRemove={onRemove}
               item={item}
             />
           ))}
       </section>
-    );
+    )
   }
 }
 
-export default Items;
+export default Items

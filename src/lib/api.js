256c9adb878fac4af708ed4c77cb26d9685b9d55
amite <amit.erandole@gmail.com>
2017-12-13 20:45:45 +0530

    added localforage api

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,51 +1,79 @@
-import localforage from 'localforage';
+import localforage from 'localforage'
+import uniqueId from 'lodash/uniqueId'
 
-window.localforage = localforage;
+localforage.config({
+  driver: localforage.WEBSQL, // Force WebSQL; same as using setDriver()
+  name: 'Packer',
+  version: 1.0,
+  size: 4980736, // Size of database, in bytes. WebSQL-only for now.
+  storeName: 'items', // Should be alphanumeric, with underscores.
+  description: 'pack your stuff'
+})
+
+const defaultState = [
+  { value: 'Pants', id: uniqueId(), packed: false },
+  { value: 'Jacket', id: uniqueId(), packed: false },
+  { value: 'iPhone Charger', id: uniqueId(), packed: false },
+  { value: 'MacBook', id: uniqueId(), packed: false },
+  { value: 'Sleeping Pills', id: uniqueId(), packed: true },
+  { value: 'Underwear', id: uniqueId(), packed: false },
+  { value: 'Hat', id: uniqueId(), packed: false },
+  { value: 'T-Shirts', id: uniqueId(), packed: false },
+  { value: 'Belt', id: uniqueId(), packed: false },
+  { value: 'Passport', id: uniqueId(), packed: true },
+  { value: 'Sandwich', id: uniqueId(), packed: true }
+]
 
 const getAll = async () => {
-  const items = await localforage.getItem('items');
-  if (!items) await localforage.setItem('items', []);
-  return items || [];
-};
+  const items =
+    (await localforage.getItem('items')) ||
+    (await localforage.setItem('items', JSON.stringify(defaultState)))
+  return items || []
+}
 
 export default {
   async add(item) {
-    const items = await getAll();
-    const newItem = { ...item, id: Date.now() };
-    localforage.setItem('items', [...items, newItem]);
-    return newItem;
+    const items = JSON.parse(await getAll())
+    const newItem = { ...item, id: uniqueId() }
+    localforage.setItem('items', JSON.stringify([newItem, ...items]))
+    return newItem
   },
 
   async getAll() {
-    return await getAll();
+    return await getAll()
   },
 
   async delete({ id }) {
-    const items = await getAll();
-    localforage.setItem('items', items.filter(item => item.id !== id));
+    const items = JSON.parse(await getAll())
+    localforage.setItem(
+      'items',
+      JSON.stringify(items.filter(item => item.id !== id))
+    )
   },
 
   async update(updatedItem) {
-    const items = await getAll();
+    const items = JSON.parse(await getAll())
     localforage.setItem(
       'items',
-      items.map(item => {
-        if (item.id === updatedItem.id) return { ...item, ...updatedItem };
-        return item;
-      }),
-    );
+      JSON.stringify(
+        items.map(item => {
+          if (item.id === updatedItem.id) return { ...item, ...updatedItem }
+          return item
+        })
+      )
+    )
   },
 
   async markAllAsUnpacked() {
-    const items = await getAll();
+    const items = JSON.parse(await getAll())
     localforage.setItem(
       'items',
-      items.map(item => ({ ...item, packed: false })),
-    );
+      JSON.stringify(items.map(item => ({ ...item, packed: false })))
+    )
   },
 
   async deleteUnpackedItems() {
-    const items = await getAll();
-    localforage.setItem('items', items.filter(({ packed }) => packed));
-  },
-};
+    const items = await getAll()
+    localforage.setItem('items', items.filter(({ packed }) => packed))
+  }
+}

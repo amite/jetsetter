d93305f44b7392871fdf495fa899851845b4a4e2
amite <amit.erandole@gmail.com>
2017-12-13 20:45:59 +0530

    added api for setState

---
+++ src/lib/logic.js
@@ -0,0 +1,32 @@
+const markAllAsUnpacked = () => ({ items }) => {
+  return {
+    items: items.map(item => {
+      return { ...item, packed: false }
+    })
+  }
+}
+
+const addItem = newItem => ({ items }) => {
+  return {
+    items: [newItem, ...items]
+  }
+}
+
+const removeItem = itemToRemove => ({ items }) => {
+  return {
+    items: items.filter(item => {
+      return item.id !== itemToRemove.id
+    })
+  }
+}
+
+const toggleItem = itemToToggle => ({ items }) => {
+  return {
+    items: items.map(item => {
+      if (item.id !== itemToToggle.id) return item
+      return { ...itemToToggle, packed: !itemToToggle.packed }
+    })
+  }
+}
+
+export { markAllAsUnpacked, addItem, removeItem, toggleItem }

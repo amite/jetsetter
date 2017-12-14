76e30bffdfc3ad1cedf955a59ed23410b33e45f4
amite <amit.erandole@gmail.com>
2017-12-14 11:48:27 +0530

    added search via render props

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,41 +1,38 @@
 import React, { Component } from 'react'
 import Item from './Item'
 import Filter from './Filter'
-import withSearch from '../hocs/withSearch'
+import Search from './Search'
 
 class Items extends Component {
   static displayName = 'Items'
 
   render() {
-    const {
-      title,
-      items,
-      onToggle,
-      onRemove,
-      searchTerm,
-      onSearchTermChange
-    } = this.props
+    const { title, items, onToggle, onRemove } = this.props
     return (
-      <section className="Items">
-        <h2>
-          {title} ({items.length})
-        </h2>
-        <Filter searchTerm={searchTerm} onChange={onSearchTermChange} />
-        {items
-          .filter(item =>
-            item.value.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-          .map(item => (
-            <Item
-              key={item.id}
-              onToggle={onToggle}
-              onRemove={onRemove}
-              item={item}
-            />
-          ))}
-      </section>
+      <Search
+        render={({ searchTerm, updateSearchTerm }) => (
+          <section className="Items">
+            <h2>
+              {title} ({items.length})
+            </h2>
+            <Filter searchTerm={searchTerm} onChange={updateSearchTerm} />
+            {items
+              .filter(item =>
+                item.value.toLowerCase().includes(searchTerm.toLowerCase())
+              )
+              .map(item => (
+                <Item
+                  key={item.id}
+                  onToggle={onToggle}
+                  onRemove={onRemove}
+                  item={item}
+                />
+              ))}
+          </section>
+        )}
+      />
     )
   }
 }
 
-export default withSearch(Items)
+export default Items

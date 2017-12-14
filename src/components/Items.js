ac5f1e6c1507ad63573088f5932dda0d0ee5dd7c
amite <amit.erandole@gmail.com>
2017-12-14 12:26:43 +0530

    refactored renderItems into separate method

--- src/components/Items.js
+++ src/components/Items.js
@@ -6,8 +6,22 @@ import Search from './Search'
 class Items extends Component {
   static displayName = 'Items'
 
+  renderItems = ({ items, searchTerm, onToggle, onRemove }) =>
+    items
+      .filter(({ value }) =>
+        value.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .map(item => (
+        <Item
+          key={item.id}
+          onToggle={onToggle}
+          onRemove={onRemove}
+          item={item}
+        />
+      ))
+
   render() {
-    const { title, items, onToggle, onRemove } = this.props
+    const { title, items } = this.props
     return (
       <Search
         render={({ searchTerm, updateSearchTerm }) => (
@@ -16,18 +30,7 @@ class Items extends Component {
               {title} ({items.length})
             </h2>
             <Filter searchTerm={searchTerm} onChange={updateSearchTerm} />
-            {items
-              .filter(item =>
-                item.value.toLowerCase().includes(searchTerm.toLowerCase())
-              )
-              .map(item => (
-                <Item
-                  key={item.id}
-                  onToggle={onToggle}
-                  onRemove={onRemove}
-                  item={item}
-                />
-              ))}
+            {this.renderItems({ ...this.props, searchTerm })}
           </section>
         )}
       />

92d13d0ae71372ef377508c274536371a38b99ad
amite <amit.erandole@gmail.com>
2017-12-20 22:15:03 +0530

    handle toggling item

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,28 +7,28 @@ import {
   MARK_ALL_AS_UNPACKED
 } from '../actions/constants'
 
-const itemsReducer = (state = [], action) => {
+const itemsReducer = (items = [], action) => {
   switch (action.type) {
     case LOAD_ITEMS:
-      return [...action.items, ...state]
+      return [...action.items, ...items]
 
     case ADD_ITEM:
-      return [action.item, ...state]
+      return [action.item, ...items]
 
     case REMOVE_ITEM:
-      return state.filter(item => item.id !== action.item.id)
+      return items.filter(item => item.id !== action.item.id)
 
     case TOGGLE_ITEM:
-      return state.map(item => {
+      return items.map(item => {
         if (item.id !== action.item.id) return item
-        return { ...action.item, packed: !action.item.packed }
+        return { ...action.item }
       })
 
     case MARK_ALL_AS_UNPACKED:
-      return state.map(item => ({ ...item, packed: false }))
+      return items.map(item => ({ ...item, packed: false }))
 
     default:
-      return state
+      return items
   }
 }
 

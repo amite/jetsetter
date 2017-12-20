9367236ca8e8e78bc24d589ec5ffa80bc1237307
amite <amit.erandole@gmail.com>
2017-12-20 11:07:16 +0530

    added redux to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
 import JetSetter from './components/Application'
 
 import './index.css'

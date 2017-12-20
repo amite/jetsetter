061be7f77312e23ba5dc5a402cb029997c80f7a0
amite <amit.erandole@gmail.com>
2017-12-20 15:39:58 +0530

    add and configure thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
 import JetSetter from './components/Application'
 
@@ -9,7 +10,10 @@ import './index.css'
 
 let store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 )
 
 ReactDOM.render(

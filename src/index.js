ba46e1fc165c97bbdc2545b11dd560e5dd982c33
amite <amit.erandole@gmail.com>
2017-12-21 09:14:03 +0530

    inject api in thunk

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
 import JetSetter from './components/Application'
-
+import api from '../src/lib/api'
 import './index.css'
 
 let store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk.withExtraArgument(api)),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 )
